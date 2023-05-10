c1e6d996f4d1c745f566833666ca11c5b580b661
RS2-BuildServer <SBG-BUILD03@aptean.com>
2023-05-10 19:58:51 +0200

    Logausgabe korrigiert, Logverzeichnis erstellen wenn es nicht existiert

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 # Remember current directory
 $current = Get-Location;
+md Log
 
 # Search for all package configrations to restore nuget packages
 $solutions = get-childitem -Filter *.sln -Recurse
@@ -17,7 +18,7 @@ $solutions | ForEach-Object -Process {
     Write-Output $_.FullName
     Set-Location -Path $_.Directory.FullName
 	# ...and compile them
-    &'C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild' /fileloggerparameters:LogFile=$current+"\Log\"+$_.Name+".log" /p:configuration=Debug $_.Name
+    &'C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild' /fileloggerparameters:LogFile=$current"\Log\"$_.Name".log" /p:configuration=Debug $_.Name
 }
 
 # restore start-directory
