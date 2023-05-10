aa444d9a7254aa2ffac28fafd2f3e164c036809d
RS2-BuildServer <SBG-BUILD03@aptean.com>
2023-05-10 18:55:37 +0200

    Buildscript umstellung auf Solutionbasierend

--- build.ps1
+++ build.ps1
@@ -2,19 +2,18 @@
 $current = Get-Location;
 
 # Search for all package configrations to restore nuget packages
-$directories = get-childitem -Filter packages.config -Recurse
-$directories | ForEach-Object -Process {
+$solutions = get-childitem -Filter *.sln -Recurse
+$solutions | ForEach-Object -Process {
     Write-Output $_.Directory.FullName
     Set-Location -Path $_.Directory.FullName
 	# start the restore
-    &'C:\Program Files (x86)\Ranorex\Studio\Bin\Addins\Misc\PackageManagement\nuget' restore -PackagesDirectory ..\packages
+    &'C:\Program Files (x86)\Ranorex\Studio\Bin\Addins\Misc\PackageManagement\nuget' restore $_.Name
 }
 # restore start-directory
 Set-Location $current
 
 # Search for all project files
-$projects = get-childitem -Filter *.csproj -Recurse
-$projects | ForEach-Object -Process {
+$solutions | ForEach-Object -Process {
     Write-Output $_.FullName
     Set-Location -Path $_.Directory.FullName
 	# ...and compile them
