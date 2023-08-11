56e874ed4a9a2b1243c18823c5c1d0d715dd521d
Gerlinde Petermaier <gerlinde.petermaier@aptean.com>
2023-08-11 15:23:53 +0200

    B_EXOPV_005 Workarounds für die unterschiedlichen Versionen wegen tw. BUG-Erledigung

--- 510/Bereich_B/B_EXOPV/B_EXOPV_005/Recordings_B_EXOPV/Fehlerprotokoll_Pruefung_kontrollieren.UserCode.cs
+++ 510/Bereich_B/B_EXOPV/B_EXOPV_005/Recordings_B_EXOPV/Fehlerprotokoll_Pruefung_kontrollieren.UserCode.cs
@@ -22,7 +22,7 @@ using Ranorex.Core.Testing;
 
 namespace B_EXOPV_005.Recordings_B_EXOPV
 {
-    public partial class Fehlerprotokoll_Pruefung_kontrollieren_511_510_wird_korrigiert
+    public partial class Fehlerprotokoll_Pruefung_kontrollieren
     {
         /// <summary>
         /// This method gets called right after the recording has been started.
--- 510/Bereich_B/B_EXOPV/B_EXOPV_005/Recordings_B_EXOPV/Fehlerprotokoll_Verbuchung_kontrollieren_510_511.UserCode.cs
+++ 510/Bereich_B/B_EXOPV/B_EXOPV_005/Recordings_B_EXOPV/Fehlerprotokoll_Verbuchung_kontrollieren_510_511.UserCode.cs
@@ -22,7 +22,7 @@ using Ranorex.Core.Testing;
 
 namespace B_EXOPV_005.Recordings_B_EXOPV
 {
-    public partial class Fehlerprotokoll_Pruefung_kontrollieren_ab_521_SP12
+    public partial class Fehlerprotokoll_Verbuchung_kontrollieren_510_511
     {
         /// <summary>
         /// This method gets called right after the recording has been started.
