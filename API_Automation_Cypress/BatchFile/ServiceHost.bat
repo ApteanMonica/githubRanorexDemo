b5bb301ae3b3ed8c189e54464739c947f50674e4
Kishore Ramachandraiah <kramachandraiah@aptean.com>
2023-09-11 06:14:49 +0000

    Updated ServiceHost.bat

--- API_Automation_Cypress/BatchFile/ServiceHost.bat
+++ API_Automation_Cypress/BatchFile/ServiceHost.bat
@@ -1,4 +1,3 @@
-@echo off
 cd /d E:\Automation\PipelineWorkingFolder
 cd /d E:\Automation\PipelineWorkingFolder\1\a\RS2Build/Release/bin
 call rs2ServiceHost.exe execute /HTTP /HTTPPORT=8080 /HTTPJSON=api
