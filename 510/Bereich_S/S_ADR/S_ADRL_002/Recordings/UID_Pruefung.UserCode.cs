b67c456663b4b118f13d85e5cfaaf354f87515c1
Monica Rizo Martinez <Monica.Rizo-Martinez@aptean.com>
2023-09-26 12:06:06 +0200

    UID Prüfung für Lieferanten

--- 510/Bereich_S/S_ADR/S_ADRL_002/Recordings/UID_Pruefung.UserCode.cs
+++ 510/Bereich_S/S_ADR/S_ADRL_002/Recordings/UID_Pruefung.UserCode.cs
@@ -20,9 +20,9 @@ using Ranorex.Core;
 using Ranorex.Core.Repository;
 using Ranorex.Core.Testing;
 
-namespace V_AUF_006
+namespace S_ADRL_002.Recordings
 {
-    public partial class Recording1
+    public partial class UID_Pruefung
     {
         /// <summary>
         /// This method gets called right after the recording has been started.
