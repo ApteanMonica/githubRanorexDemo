94f51a72dbbd2092eb2555d7943d663faaebbec9
Gerlinde Petermaier <gerlinde.petermaier@aptean.com>
2024-02-06 09:17:13 +0100

    b_buda_001: Fehler 521 row Bereichsaufteilung: zusätzl. Waitfor Tabelle Bereichsaufteilung eingebaut

--- 510/Bereich_B/B_BUDA/B_BUDA_001/Recordings_B_BUCH/Dauerbuchungsvorlage_erfassen_SA1_Bereichsaufteilung.cs
+++ 510/Bereich_B/B_BUDA/B_BUDA_001/Recordings_B_BUCH/Dauerbuchungsvorlage_erfassen_SA1_Bereichsaufteilung.cs
@@ -85,47 +85,47 @@ namespace B_BUDA_001.Recordings_B_BUCH
             Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'MdiBuch.PbStandard7' at Center.", repo.MdiBuch.PbStandard7Info, new RecordItemIndex(1));
             repo.MdiBuch.PbStandard7.Click();
             
-            Report.Log(ReportLevel.Info, "Wait", "Waiting 3m to exist. Associated repository item: 'MdiBuch.frmSB.Row1'", repo.MdiBuch.frmSB.Row1Info, new ActionTimeout(180000), new RecordItemIndex(2));
-            repo.MdiBuch.frmSB.Row1Info.WaitForExists(180000);
+            Report.Log(ReportLevel.Info, "Wait", "Waiting 3m to exist. Associated repository item: 'MdiBuch.FlexGrid_Bereichsaufteilung'", repo.MdiBuch.FlexGrid_BereichsaufteilungInfo, new ActionTimeout(180000), new RecordItemIndex(2));
+            repo.MdiBuch.FlexGrid_BereichsaufteilungInfo.WaitForExists(180000);
             
-            //Report.Log(ReportLevel.Info, "Mouse", "Mouse Left DoubleClick item 'MdiBuch.frmSB.colBube_betragRow1' at Center.", repo.MdiBuch.frmSB.colBube_betragRow1Info, new RecordItemIndex(3));
-            //repo.MdiBuch.frmSB.colBube_betragRow1.DoubleClick();
+            Report.Log(ReportLevel.Info, "Wait", "Waiting 2m to exist. Associated repository item: 'MdiBuch.Row1'", repo.MdiBuch.Row1Info, new ActionTimeout(120000), new RecordItemIndex(3));
+            repo.MdiBuch.Row1Info.WaitForExists(120000);
             
-            //Report.Log(ReportLevel.Info, "Keyboard", "Key 'Delete' Press.", new RecordItemIndex(4));
+            //Report.Log(ReportLevel.Info, "Mouse", "Mouse Left DoubleClick item 'MdiBuch.colBube_betragRow1' at Center.", repo.MdiBuch.colBube_betragRow1Info, new RecordItemIndex(4));
+            //repo.MdiBuch.colBube_betragRow1.DoubleClick();
+            
+            //Report.Log(ReportLevel.Info, "Keyboard", "Key 'Delete' Press.", new RecordItemIndex(5));
             //Keyboard.Press(System.Windows.Forms.Keys.Delete, Keyboard.DefaultScanCode, Keyboard.DefaultKeyPressTime, 1, true);
             
-            Report.Log(ReportLevel.Info, "Keyboard", "Key sequence '499,99' with focus on 'MdiBuch.frmSB.colBube_betragRow1'.", repo.MdiBuch.frmSB.colBube_betragRow1Info, new RecordItemIndex(5));
-            repo.MdiBuch.frmSB.colBube_betragRow1.PressKeys("499,99");
+            Report.Log(ReportLevel.Info, "Keyboard", "Key sequence '499,99' with focus on 'MdiBuch.colBube_betragRow1'.", repo.MdiBuch.colBube_betragRow1Info, new RecordItemIndex(6));
+            repo.MdiBuch.colBube_betragRow1.PressKeys("499,99");
             
-            Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(6));
+            Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(7));
             Keyboard.Press(System.Windows.Forms.Keys.Tab, Keyboard.DefaultScanCode, Keyboard.DefaultKeyPressTime, 1, true);
             
-            Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (Text='499,99') on item 'MdiBuch.frmSB.colBube_betragRow1'.", repo.MdiBuch.frmSB.colBube_betragRow1Info, new RecordItemIndex(7));
-            Validate.AttributeEqual(repo.MdiBuch.frmSB.colBube_betragRow1Info, "Text", "499,99");
-            
-            Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(8));
-            Keyboard.Press(System.Windows.Forms.Keys.Tab, Keyboard.DefaultScanCode, Keyboard.DefaultKeyPressTime, 1, true);
+            Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (Text='499,99') on item 'MdiBuch.colBube_betragRow1'.", repo.MdiBuch.colBube_betragRow1Info, new RecordItemIndex(8));
+            Validate.AttributeEqual(repo.MdiBuch.colBube_betragRow1Info, "Text", "499,99");
             
             Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(9));
             Keyboard.Press(System.Windows.Forms.Keys.Tab, Keyboard.DefaultScanCode, Keyboard.DefaultKeyPressTime, 1, true);
             
-            Report.Log(ReportLevel.Info, "Keyboard", "Key sequence '30' with focus on 'MdiBuch.frmSB.ColBubeBektnr1Row1'.", repo.MdiBuch.frmSB.ColBubeBektnr1Row1Info, new RecordItemIndex(10));
-            repo.MdiBuch.frmSB.ColBubeBektnr1Row1.PressKeys("30");
-            
-            Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(11));
+            Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(10));
             Keyboard.Press(System.Windows.Forms.Keys.Tab, Keyboard.DefaultScanCode, Keyboard.DefaultKeyPressTime, 1, true);
             
-            Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (Text='KST 30') on item 'MdiBuch.frmSB.ColBezRow1'.", repo.MdiBuch.frmSB.ColBezRow1Info, new RecordItemIndex(12));
-            Validate.AttributeEqual(repo.MdiBuch.frmSB.ColBezRow1Info, "Text", "KST 30");
+            Report.Log(ReportLevel.Info, "Keyboard", "Key sequence '30' with focus on 'MdiBuch.ColBubeBektnr1Row1'.", repo.MdiBuch.ColBubeBektnr1Row1Info, new RecordItemIndex(11));
+            repo.MdiBuch.ColBubeBektnr1Row1.PressKeys("30");
             
-            Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(13));
+            Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(12));
             Keyboard.Press(System.Windows.Forms.Keys.Tab, Keyboard.DefaultScanCode, Keyboard.DefaultKeyPressTime, 1, true);
             
-            Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (Text='500,01') on item 'MdiBuch.frmSB.colBube_betragRow2'.", repo.MdiBuch.frmSB.colBube_betragRow2Info, new RecordItemIndex(14));
-            Validate.AttributeEqual(repo.MdiBuch.frmSB.colBube_betragRow2Info, "Text", "500,01");
+            Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (Text='KST 30') on item 'MdiBuch.ColBezRow1'.", repo.MdiBuch.ColBezRow1Info, new RecordItemIndex(13));
+            Validate.AttributeEqual(repo.MdiBuch.ColBezRow1Info, "Text", "KST 30");
             
-            Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(15));
+            Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(14));
             Keyboard.Press(System.Windows.Forms.Keys.Tab, Keyboard.DefaultScanCode, Keyboard.DefaultKeyPressTime, 1, true);
+            
+            Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (Text='500,01') on item 'MdiBuch.colBube_betragRow2'.", repo.MdiBuch.colBube_betragRow2Info, new RecordItemIndex(15));
+            Validate.AttributeEqual(repo.MdiBuch.colBube_betragRow2Info, "Text", "500,01");
             
             Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(16));
             Keyboard.Press(System.Windows.Forms.Keys.Tab, Keyboard.DefaultScanCode, Keyboard.DefaultKeyPressTime, 1, true);
@@ -133,19 +133,22 @@ namespace B_BUDA_001.Recordings_B_BUCH
             Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(17));
             Keyboard.Press(System.Windows.Forms.Keys.Tab, Keyboard.DefaultScanCode, Keyboard.DefaultKeyPressTime, 1, true);
             
-            Report.Log(ReportLevel.Info, "Keyboard", "Key sequence '50' with focus on 'MdiBuch.frmSB.ColBubeBektnr1Row2'.", repo.MdiBuch.frmSB.ColBubeBektnr1Row2Info, new RecordItemIndex(18));
-            repo.MdiBuch.frmSB.ColBubeBektnr1Row2.PressKeys("50");
-            
-            Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(19));
+            Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(18));
             Keyboard.Press(System.Windows.Forms.Keys.Tab, Keyboard.DefaultScanCode, Keyboard.DefaultKeyPressTime, 1, true);
             
-            Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (Text='KST 50') on item 'MdiBuch.frmSB.ColBezRow2'.", repo.MdiBuch.frmSB.ColBezRow2Info, new RecordItemIndex(20));
-            Validate.AttributeEqual(repo.MdiBuch.frmSB.ColBezRow2Info, "Text", "KST 50");
+            Report.Log(ReportLevel.Info, "Keyboard", "Key sequence '50' with focus on 'MdiBuch.ColBubeBektnr1Row2'.", repo.MdiBuch.ColBubeBektnr1Row2Info, new RecordItemIndex(19));
+            repo.MdiBuch.ColBubeBektnr1Row2.PressKeys("50");
             
-            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'MdiBuch.PbStandard2' at Center.", repo.MdiBuch.PbStandard2Info, new RecordItemIndex(21));
-            repo.MdiBuch.PbStandard2.Click();
+            Report.Log(ReportLevel.Info, "Keyboard", "Key 'Tab' Press.", new RecordItemIndex(20));
+            Keyboard.Press(System.Windows.Forms.Keys.Tab, Keyboard.DefaultScanCode, Keyboard.DefaultKeyPressTime, 1, true);
+            
+            Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (Text='KST 50') on item 'MdiBuch.ColBezRow2'.", repo.MdiBuch.ColBezRow2Info, new RecordItemIndex(21));
+            Validate.AttributeEqual(repo.MdiBuch.ColBezRow2Info, "Text", "KST 50");
             
             Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'MdiBuch.PbStandard2' at Center.", repo.MdiBuch.PbStandard2Info, new RecordItemIndex(22));
+            repo.MdiBuch.PbStandard2.Click();
+            
+            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'MdiBuch.PbStandard2' at Center.", repo.MdiBuch.PbStandard2Info, new RecordItemIndex(23));
             repo.MdiBuch.PbStandard2.Click();
             
         }
